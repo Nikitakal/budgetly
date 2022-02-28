b8731cc6b6b42e06eacddc03ca57669448a781b4
Nikita Kalashnikov <nickkalashnikov96@gmail.com>
2022-02-27 16:31:26 -0800

    Added title and btns. Prepared env for cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import { Container } from "react-bootstrap";
+import { Button, Container, Stack } from "react-bootstrap";
 
 function App() {
-  return <Container></Container>;
+  return (
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      ></div>
+    </Container>
+  );
 }
 
 export default App;
