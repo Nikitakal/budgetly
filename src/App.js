fc3a430120773c8a76dc9d09e435634f461946ac
Nikita Kalashnikov <nickkalashnikov96@gmail.com>
2022-03-01 15:53:01 -0800

    Added budgets and ability to store them in history

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
 import { Button, Container, Stack } from "react-bootstrap";
 import BudgetCard from "./components/BudgetCard";
+import AddBudgetModal from "./components/AddBudgetModal";
+import { useState } from "react";
+import { useBudgets } from "./contexts/BudgetsContext";
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  const { budgets, getBudgetExpenses } = useBudgets();
   return (
-    <Container className="my-4">
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        {/* me-auto makes it so that Budgets is always on the left side of our screen */}
-        <h1 className="me-auto">Budgets</h1>
-        <Button variant="primary">Add Budget</Button>
-        <Button variant="outline-primary">Add Expense</Button>
-      </Stack>
-      {/* Using CSS Grids */}
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-          gap: "1rem",
-          // Using flex-start, because not all of our cards will have the same size
-          alignItems: "flex-start",
-        }}
-      >
-        {/* Added some placeholder values to visualize the code */}
-        <BudgetCard name="Entertainment" amount={200} max={1000}></BudgetCard>
-      </div>
-    </Container>
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          {/* me-auto makes it so that Budgets is always on the left side of our screen */}
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
+            Add Budget
+          </Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        {/* Using CSS Grids */}
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+            gap: "1rem",
+            // Using flex-start, because not all of our cards will have the same size
+            alignItems: "flex-start",
+          }}
+        >
+          {/* Get all of my expenses, add all of the amounts together, return as amount */}
+          {budgets.map((budget) => {
+            const amount = getBudgetExpenses(budget.id).reduce(
+              (total, expense) => total + expense.amount,
+              0
+            );
+            return (
+              <BudgetCard
+                key={budget.id}
+                name={budget.name}
+                amount={amount}
+                max={budget.max}
+              />
+            );
+          })}
+        </div>
+      </Container>
+      <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={() => setShowAddBudgetModal(false)}
+      />
+    </>
   );
 }
 
